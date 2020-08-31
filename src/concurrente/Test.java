91c615d2f3c41328d0452961c7a6dd39d59b0135
Fernando Iraira <fmiraira@gmail.com>
2020-08-31 15:54:40 -0300

    Se borro obtenerNodo, porque por el momento no tengo ganas de estudiar

---
+++ src/concurrente/Test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concurrente;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        PingPong t1 = new PingPong("PING", 33);
+        PingPong t2 = new PingPong("PONG", 10);
+        
+        t1.start();
+        t2.start();
+
+    }
+
+}
