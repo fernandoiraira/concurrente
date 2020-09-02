cd709658505a82337903588a3544210513ad6268
Fernando Iraira <fmiraira@gmail.com>
2020-09-02 12:39:35 -0300

    Se cambio el nombre del paquete

---
+++ src/TP2/ej1/Recurso.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP2.ej1;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Recurso {
+
+    static void uso() {
+        Thread t = Thread.currentThread();
+
+        System.out.println("en Recurso: Soy" + t.getName());
+    }
+}
