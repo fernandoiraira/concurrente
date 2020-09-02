c147c63f3debef0a73ef5010cdc8c8a21ff17492
Fernando Iraira <fmiraira@gmail.com>
2020-09-02 15:11:01 -0300

    Creacion de la clase Main

---
+++ src/TP2/ej6/main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP2.ej6;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class main {
+
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Cliente 1", new int[]{2, 2, 1, 5, 2, 3});
+        Cliente cliente2 = new Cliente("Cliente 2", new int[]{1, 3, 5, 1, 1});
+        Cajera cajera1 = new Cajera("Cajera 1");
+        
+        long initialTime = System.currentTimeMillis();
+        cajera1.procesarCompra(cliente1, initialTime);
+        cajera1.procesarCompra(cliente2, initialTime);
+    }
+
+}
