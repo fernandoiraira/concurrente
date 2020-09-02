0d1356ff003084e8a8624d6c34b0fe8670807730
Fernando Iraira <fmiraira@gmail.com>
2020-09-02 13:43:15 -0300

    Correccion menor

--- src/TP2/ej3/Recurso.java
+++ src/TP2/ej3/Recurso.java
@@ -14,7 +14,7 @@ public class Recurso {
     static void uso() {
         Thread t = Thread.currentThread();
 
-        System.out.println("en Recurso: Soy" + t.getName());
+        System.out.println("en Recurso: Soy " + t.getName());
     }
 
 }
