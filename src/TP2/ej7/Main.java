84c2c42e6bfab6f46b684a1e21f3eab62d4a2d7f
Fernando Iraira <fmiraira@gmail.com>
2020-09-02 15:36:59 -0300

    Creacion de la clase Main

---
+++ src/TP2/ej7/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP2.ej7;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
