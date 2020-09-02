32485e115033fdcefda85fd09dfe376a17f12489
Fernando Iraira <fmiraira@gmail.com>
2020-09-02 12:10:07 -0300

    Creacion de la clase PruebaRunnable

---
+++ src/EjemploRunnable/PruebaRunnable.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjemploRunnable;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class PruebaRunnable {
+
+    public static void main(String[] args) {
+        //2 objetos definen los metodos run
+        PingPong o1 = newPingPong("PING", 33);
+        PingPong o2 = newPingPong("PONG", 10);
+
+        //Se crean los hilos
+        Thread t1 = new Thread(o1);
+        Thread t2 = new Thread(o2);
+
+        //Se activan los hilos
+        t1.start();
+        t2.start();
+    }
+
+}
