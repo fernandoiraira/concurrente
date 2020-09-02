e4a15c01dbf8040a72404e9c7e72f2c0449b9017
Fernando Iraira <fmiraira@gmail.com>
2020-09-02 14:48:21 -0300

    Creacion de la clase Cajera

---
+++ src/TP2/ej6/Cajera.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP2.ej6;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Cajera {
+
+    private String nombre;
+
+    public Cajera(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void procesarCompra(Cliente cliente, long timeStamp) {
+        
+
+    }
+
+}
