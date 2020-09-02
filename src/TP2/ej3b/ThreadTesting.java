56bf5500b9c17e60b79df655301386b2ccc54efc
Fernando Iraira <fmiraira@gmail.com>
2020-09-02 14:34:36 -0300

    Creacion de la clase ThreadTesting

---
+++ src/TP2/ej3b/ThreadTesting.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP2.ej3b;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class ThreadTesting {
+
+    public static void main(String[] args) {
+        MiEjecucion h1 = new MiEjecucion();
+
+        Thread t1 = new Thread(h1);
+
+        t1.start();
+
+        try {
+            t1.join();
+
+        } catch (Exception e) {
+
+        }
+
+        System.out.println("En el main");
+
+    }
+
+}
