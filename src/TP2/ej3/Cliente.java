db9564d557d2a4657f5ee23deeb79024432f2e3a
Fernando Iraira <fmiraira@gmail.com>
2020-09-02 13:10:51 -0300

    Se agrego llamado al metodo Recurso.uso()

--- src/TP2/ej3/Cliente.java
+++ src/TP2/ej3/Cliente.java
@@ -14,6 +14,7 @@ public class Cliente implements Runnable {
     @Override
     public void run() {
         System.out.println("soy " + Thread.currentThread().getName());
+        Recurso.uso();
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
