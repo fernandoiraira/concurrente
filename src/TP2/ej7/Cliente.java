8b3708610ed509bad7fc68ee1c6f0d86fc074266
Fernando Iraira <fmiraira@gmail.com>
2020-09-02 15:25:26 -0300

    Creacion de la clase Cliente

---
+++ src/TP2/ej7/Cliente.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP2.ej7;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Cliente {
+
+    private String nombre;
+    private int[] carroCompra;
+
+    public Cliente(String nombre, int[] carrito) {
+        this.nombre = nombre;
+        this.carroCompra = carrito;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public int[] getCarroCompra() {
+        return this.carroCompra;
+    }
+}
