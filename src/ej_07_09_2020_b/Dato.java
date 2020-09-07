997df6cd989d35b286e662d4df61ba17a7db507e
Fernando Iraira <fmiraira@gmail.com>
2020-09-07 14:54:04 -0300

    Copiado de la clase Dato

---
+++ src/ej_07_09_2020_b/Dato.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej_07_09_2020_b;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+class Dato {
+
+    private int valor;
+
+    void contar() {
+        valor = ++valor;
+    }
+
+    int obtenerValor() {
+        return valor;
+    }
+
+}
