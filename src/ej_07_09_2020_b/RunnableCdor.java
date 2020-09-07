d5485b758f49fc27974595817f9acc7ec4192588
Fernando Iraira <fmiraira@gmail.com>
2020-09-07 14:52:37 -0300

    Creacion de la clase

---
+++ src/ej_07_09_2020_b/RunnableCdor.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ej_07_09_2020_b;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+class RunnableCdor implements Runnable {
+
+Dato unContador;  
+
+public RunnableCdor (Dato elCdor){
+
+unContador = elCdor);
+
+}
+
+
+public void run() {
+
+for (int x=0; x<10000; ++x)
+
+unContador.incrementar();
+
+}
+
+}
