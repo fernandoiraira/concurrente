71dc26b1b1763fe8f474d32b7ca47042f3b59373
Fernando Iraira <fmiraira@gmail.com>
2020-09-07 14:54:40 -0300

    se cambio getValor por obtenerValor

--- src/ej_07_09_2020_b/Contador.java
+++ src/ej_07_09_2020_b/Contador.java
@@ -3,31 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ej_07_09_2020_b;
 
 /**
- * 
+ *
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
 public class Contador {
 
+    public static void main(String[] args) throws InterruptedException {
 
-public static void main(String[] args) throws InterruptedException {
+        final Dato unContador = new Dato();
 
-final Dato unContador = new Dato();
+        RunnableCdor unRunCdor = new RunnableCdor(unContador);
 
-RunnableCdor   unRunCdor = new RunnableCdor(unContador);
+        Thread h1 = new Thread(unRunCdor);
+        Thread h2 = new Thread(unRunCdor);
+        h1.start();
+        h2.start();
+        h1.join();
+        h2.join();
+        System.out.println("en main-" + unContador.obtenerValor());
 
-
-Thread h1= new Thread( unRunCdor);
-Thread h2= new Thread( unRunCdor);
-h1.start();
-h2.start();
-h1.join();
-h2.join();
-System.out.println("en main-" + unContador.getValor());
-
-}
+    }
 
 }
