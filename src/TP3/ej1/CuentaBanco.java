6c44ca0e66268794d787c2f5c02bad13ceaabe72
Fernando Iraira <fmiraira@gmail.com>
2020-09-09 18:36:34 -0300

    Creacion de la clase CuentaBanco

---
+++ src/TP3/ej1/CuentaBanco.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP3.ej1;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class CuentaBanco {
+
+    private int balance = 50;
+
+    public CuentaBanco() {
+
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void retiroBancario(int retiro) {
+        balance = balance - retiro;
+    }
+}
