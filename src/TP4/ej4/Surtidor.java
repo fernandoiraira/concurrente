3e18ad8a2159167fd7fcc085f0bb5de1bbb07a9d
Fernando Iraira <fmiraira@gmail.com>
2020-09-11 15:19:52 -0300

    Se agrego el metodo llenar

---
+++ src/TP4/ej4/Surtidor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package TP4.ej4;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Surtidor {
+    
+    public synchronized void cargar(Auto auto){
+        auto.llenar();        
+    }
+    
+    
+
+}
