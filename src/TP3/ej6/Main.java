ec38cfbe73acebd1ce1ee5cb078da0d156ec4bc0
Fernando Iraira <fmiraira@gmail.com>
2020-09-11 19:05:49 -0300

    Se le cambio el nombre al paquete

---
+++ src/TP3/ej6/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP3.ej6;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        int k = 3; //Numero de Hilos
+        int tope = 7;
+        int c = 0;
+        HiloSumador[] hilo = new HiloSumador[k];
+        int[] array = new int[tope];
+        Sync sync = new Sync(1);
+
+        for (int i = 0; i <= tope - 1; i++) {
+            array[i] = 2;
+        }
+
+        while ((tope - (c - 1) > k)) {
+            hilo[c] = new HiloSumador(array, c, c + ((tope / k) - 1), sync); //int[] array, int inicio, int fin, Sync sync
+            c = c + ((tope / k) - 1);
+        }
+
+        while (!sync.termino()) {
+
+        }
+
+        System.out.println("Suma total: " + sync.getSuma());
+
+    }
+}
---
+++ src/TP3/ej6/Sync.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP3.ej6;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Sync {
+
+    int sumaTotal = 0;
+    int contador = 0;
+    int tope;
+
+    public Sync(int tope) {
+        this.tope = tope;
+    }
+
+    public boolean termino() {
+        return this.contador == tope;
+    }
+
+    public synchronized void sumar(int suma) {
+        this.sumaTotal = suma;
+        this.contador++;
+    }
+
+    public int getSuma() {
+        return this.sumaTotal;
+    }
+
+}
