7a261e082c013e280e087ed8970fecd466f66e9d
Fernando Iraira <fmiraira@gmail.com>
2020-09-11 18:13:06 -0300

    Se agrego el metodo sumar

--- src/TP4/ej6/Sync.java
+++ src/TP4/ej6/Sync.java
@@ -11,7 +11,7 @@ package TP4.ej6;
  */
 public class Sync {
 
-    int sumaTotal=0;
+    int sumaTotal = 0;
     int contador = 0;
     int tope;
 
@@ -21,6 +21,10 @@ public class Sync {
 
     public boolean termino() {
         return this.contador == tope;
+    }
+
+    public synchronized void sumar(int suma) {
+        this.sumaTotal = suma;
     }
 
 }
