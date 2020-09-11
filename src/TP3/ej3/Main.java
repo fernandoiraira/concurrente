697044469b4297a120ebd9784fd6dfccefd2b349
Fernando Iraira <fmiraira@gmail.com>
2020-09-11 17:06:05 -0300

    Se corrio el inicio de los hilos

--- src/TP3/ej3/Main.java
+++ src/TP3/ej3/Main.java
@@ -16,14 +16,15 @@ public class Main {
 
         Letra l1 = new Letra("A", coord);
         Thread a = new Thread(l1);
-        a.start();
 
         Letra l2 = new Letra("BB", coord);
         Thread b = new Thread(l2);
-        b.start();
 
         Letra l3 = new Letra("CCC", coord);
         Thread c = new Thread(l3);
+
+        a.start();
+        b.start();
         c.start();
 
     }
