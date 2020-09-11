7442da8af1bddf369d51cef8d4277b87c370bb58
Fernando Iraira <fmiraira@gmail.com>
2020-09-11 17:04:03 -0300

    Se agrego un bucle for para crear autos

--- src/TP4/ej4/Main.java
+++ src/TP4/ej4/Main.java
@@ -12,7 +12,14 @@ package TP4.ej4;
 public class Main {
 
     public static void main(String[] args) {
-        
+        Surtidor ypf = new Surtidor();
+        for (int i = 0; i < 6; i++) {
+            Auto a = new Auto(Integer.toString(i), i, Integer.toString(i), ypf);//patente, int modelo, String marca, Surtidor surtidor
+            Thread aT = new Thread(a);
+            
+            aT.start();
+
+        }
 
     }
 
