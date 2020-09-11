56ab675664595556ee589a7f09b8711f07c5df39
Fernando Iraira <fmiraira@gmail.com>
2020-09-11 12:52:35 -0300

    Creacion de la clase

--- src/TP3/ej3/Letra.java
+++ src/TP3/ej3/Letra.java
@@ -3,21 +3,30 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package TP3.ej3;
 
 /**
- * 
+ *
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
-public class Letra {
+public class Letra implements Runnable {
+
     private char letra;
-    
-    public Letra(char letra){
+    private Coordinador coordinador;
+
+    public Letra(char letra, Coordinador coordinador) {
+        this.letra = letra;
+        this.coordinador = coordinador;
+    }
+
+    public Letra(char letra) {
         this.letra = letra;
     }
-    
-    
-           
+
+    public void run() {
+        while (!coordinador.esTurno(letra)) {
+            System.out.println(letra);
+        }
+    }
 
 }
