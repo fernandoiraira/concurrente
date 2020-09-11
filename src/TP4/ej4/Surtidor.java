1a7f0bfd8d1819d6df1c771096b4aee8f995f70a
Fernando Iraira <fmiraira@gmail.com>
2020-09-11 16:48:06 -0300

    Se agrego sleep para simular el tiempo de carga de nafta

--- src/TP4/ej4/Surtidor.java
+++ src/TP4/ej4/Surtidor.java
@@ -3,19 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package TP4.ej4;
 
 /**
- * 
+ *
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
 public class Surtidor {
-    
-    public synchronized void cargar(Auto auto){
-        auto.llenar();        
+
+    public synchronized void cargar(Vehiculo auto) {
+        System.out.println("Se esta cargando el auto: " + auto.getMatricula());
+        auto.llenar();
+        try {
+            Thread.sleep(700);
+        } catch (Exception e) {
+
+        }
+        System.out.println("Se cargo el auto: " + auto.getMatricula());
     }
-    
-    
 
 }
