e2d7f6b7c9dd3f3b4bcaae1836aecfa068214728
Fernando Iraira <fmiraira@gmail.com>
2020-09-11 12:28:58 -0300

    Creacion de la clase

---
+++ src/TP3/ej3/Coordinador.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP3.ej3;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Coordinador {
+
+    private char[] lista = {'A', 'B', 'C'};
+
+}
