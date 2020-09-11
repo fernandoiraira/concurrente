cbabc0657979606fc49846215801656135690b06
Fernando Iraira <fmiraira@gmail.com>
2020-09-11 15:16:09 -0300

    Creacion de la clase

---
+++ src/TP4/ej4/Auto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP4.ej4;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Auto extends Vehiculo {
+
+    public Auto(String patente, int modelo, String marca, int kmFaltantesParaElService) {
+        super(patente, modelo, marca, kmFaltantesParaElService);
+    }
+
+}
