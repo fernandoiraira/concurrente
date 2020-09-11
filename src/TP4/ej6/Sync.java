09c89b832634ac6606f4ea9cfe4338d85bbd3f95
Fernando Iraira <fmiraira@gmail.com>
2020-09-11 18:01:49 -0300

    Creacion de la clase Sync

---
+++ src/TP4/ej6/Sync.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP4.ej6;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Sync {
+
+    int contador = 0;
+    int tope;
+
+    public Sync(int tope) {
+        this.tope = tope;
+    }
+
+    public boolean termino() {
+        return this.contador == tope;
+    }
+
+}
