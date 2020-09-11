dc85d36d1d76ebbf4bdb0711dfbfafaa360b7147
Fernando Iraira <fmiraira@gmail.com>
2020-09-11 12:41:56 -0300

    Creacion de la clase

---
+++ src/TP3/ej3/Letra.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package TP3.ej3;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Letra {
+    private char letra;
+    
+    public Letra(char letra){
+        this.letra = letra;
+    }
+    
+    
+           
+
+}
