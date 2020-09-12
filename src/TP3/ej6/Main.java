2ee9896e960fcb77ba3cf70a32f73b95944a3dc4
Fernando Iraira <fmiraira@gmail.com>
2020-09-11 23:03:19 -0300

    Se agrego un for que crea los hilos, menos el ultimo, ya que este puede tener mas o menos sectores del arreglo que le corresponden

--- src/TP3/ej6/Main.java
+++ src/TP3/ej6/Main.java
@@ -15,14 +15,27 @@ public class Main {
 
         int k = 3; //Numero de Hilos
         int tope = 50000;
-        int alcance = tope/k;
+        int alcance = tope / k;
         int c = 0;
-        HiloSumador[] hilo = new HiloSumador[k];
+        Thread[] hilo = new Thread[k];
         int[] array = new int[tope];
         Sync sync = new Sync(1);
 
         for (int i = 0; i <= tope - 1; i++) {
             array[i] = 2;
+        }
+
+        for (int i = 1; i <= k - 1; i++) {
+            HiloSumador h = new HiloSumador(array, c, c + alcance, sync); //int[] array, int inicio, int fin, Sync sync
+            hilo[i] = new Thread(h);
+            c = c + alcance;
+        }
+
+        HiloSumador h = new HiloSumador(array, tope - c, tope - 1, sync); //int[] array, int inicio, int fin, Sync sync
+        hilo[k - 1] = new Thread(h);
+
+        for (int i = 0; i < k - 1; i++) {
+            hilo[i].start();
         }
 
         while (!sync.termino()) {
