db267c278f100938dea143575cddfce6e9165520
Fernando Iraira <fmiraira@gmail.com>
2020-09-17 11:05:18 -0300

    Creacion de la clase PROCESOI

---
+++ src/EjemploSemaforo/ProcesoI.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjemploSemaforo;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class ProcesoI implements Runnable {
+
+    private Datos unDato;
+
+    public ProcesoI(Datos unD) {
+        unDato = unD;
+    }
+
+    public void run() {
+        for (int i = 1; i < 10000; i++) {
+            unDato.incrementar();
+        }
+    }
+
+}
