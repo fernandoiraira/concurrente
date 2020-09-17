b363d9daf8b995f22e1a765fc3a341c3e211aaa1
Fernando Iraira <fmiraira@gmail.com>
2020-09-17 17:08:39 -0300

    Commit final

--- src/EjemploSemaforo/Main.java
+++ src/EjemploSemaforo/Main.java
@@ -12,7 +12,20 @@ package EjemploSemaforo;
 public class Main {
 
     public static void main(String[] args) {
+        Datos dato = new Datos(0);
+
+        for (int i = 1; i <= 2; i++) {
+            ProcesoI p = new ProcesoI(dato);
+            Thread t = new Thread(p);
+
+            t.start();
+        }
+
+        try {
+            Thread.sleep(3000);
+        } catch (Exception e) {
+
+        }
 
     }
-
 }
