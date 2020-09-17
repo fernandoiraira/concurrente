996ce2651b8dc0176926756eab8b121dce521704
Fernando Iraira <fmiraira@gmail.com>
2020-09-17 11:12:29 -0300

    Creacion de la clase Datos

---
+++ src/EjemploSemaforo/Datos.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjemploSemaforo;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Datos {
+
+    private int dato;
+    private Semaphore mutex;
+
+    public Datos(int nro) {
+        dato = nro;
+        mutex = new Semaphore(1);
+    }
+
+    public int getDato() {
+        return dato;
+    }
+
+    public void incrementar() {
+
+        try {
+            mutex.acquire();
+        } catch (Exception e) {
+
+        }
+
+        dato++;
+        mutex.release();
+    }
+
+}
