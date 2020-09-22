9aacd91e45474887f7fbf02ffa9d8d1667686a44
Fernando Iraira <fmiraira@gmail.com>
2020-09-22 17:41:48 -0300

    Creacion de la clase Cliente

---
+++ src/Barbero_Semaforo/Cliente.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Barbero_Semaforo;
+
+/**
+ *
+ * @author Fernando Iraira <fmiraira@gmail.com>
+ */
+public class Cliente implements Runnable {
+
+    public void run() {
+
+    }
+
+}
