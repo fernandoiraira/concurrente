b9920d9226848c5b5dcbbcf871872c4d6e758214
Fernando Iraira <fmiraira@gmail.com>
2020-09-22 17:44:46 -0300

    Se creo el recurso compartido Sillon, con los semaforos necesarios

---
+++ src/Barbero_Semaforo/Sillon.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Barbero_Semaforo;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ *
+ * @author Fernando Iraira <fmiraira@gmail.com>
+ */
+public class Sillon {
+
+    private Semaphore semSillon = new Semaphore(1);
+    private Semaphore semBarbero = new Semaphore(0);
+    private Semaphore semSalida = new Semaphore(0);
+    
+    
+
+}
