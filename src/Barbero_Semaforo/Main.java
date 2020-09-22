4bd0239367d6222b11a37e8f5a80b270961d65d3
Fernando Iraira <fmiraira@gmail.com>
2020-09-22 17:53:14 -0300

    Creacion de main de prueba

---
+++ src/Barbero_Semaforo/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Barbero_Semaforo;
+
+/**
+ *
+ * @author Fernando Iraira <fmiraira@gmail.com>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            Cliente c1 = new Cliente();
+            Thread t = new Thread(c1, "Cliente " + i);
+            t.start();
+        }
+
+        Sillon sillon = new Sillon();
+        
+        
+
+    }
+
+}
