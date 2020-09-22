9d1046e5eef309deec89359c62cd254ca5c4b3df
Fernando Iraira <fmiraira@gmail.com>
2020-09-22 17:56:29 -0300

    Ahora se pasa por parametro el recurso compartido

--- src/Barbero_Semaforo/Main.java
+++ src/Barbero_Semaforo/Main.java
@@ -12,15 +12,13 @@ package Barbero_Semaforo;
 public class Main {
 
     public static void main(String[] args) {
+        Sillon sillon = new Sillon();
+
         for (int i = 0; i < 10; i++) {
-            Cliente c1 = new Cliente();
+            Cliente c1 = new Cliente(sillon);
             Thread t = new Thread(c1, "Cliente " + i);
             t.start();
         }
-
-        Sillon sillon = new Sillon();
-        
-        
 
     }
 
