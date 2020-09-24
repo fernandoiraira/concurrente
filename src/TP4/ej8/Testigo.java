2120c8eca7a1b781aeb062a081659758ce8ee7b9
Fernando Iraira <fmiraira@gmail.com>
2020-09-24 17:16:14 -0300

    Creacion de la clase Testigo

---
+++ src/TP4/ej8/Testigo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package TP4.ej8;
+
+/**
+ * 
+ * @author Fernando Iraira <fmiraira@gmail.com>
+ */
+public class Testigo {
+
+}
