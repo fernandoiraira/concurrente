e5172103a70311c4d3cbf648e0032d092d2c4cc3
Fernando Iraira <fmiraira@gmail.com>
2020-09-24 17:17:01 -0300

    Creacion de la clase Atleta

---
+++ src/TP4/ej8/Atleta.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP4.ej8;
+
+/**
+ *
+ * @author Fernando Iraira <fmiraira@gmail.com>
+ */
+public class Atleta implements Runnable {
+
+    public void run() {
+
+    }
+}
