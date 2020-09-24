d75aad8c1e52160d23488e17f060802dec566caa
Fernando Iraira <fmiraira@gmail.com>
2020-09-24 17:15:47 -0300

    Creacion de la clase Carrera

---
+++ src/TP4/ej8/Carrera.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP4.ej8;
+
+/**
+ *
+ * @author Fernando Iraira <fmiraira@gmail.com>
+ */
+public class Carrera {
+
+    public static void main(String[] args) {
+
+    }
+
+}
